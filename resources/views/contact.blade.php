050679b48f4b0661658fe6004e4cf0a925b41d98
Jorda Coca <jorda.coca@estudiants.urv.cat>
2025-01-22 20:42:09 +0100

    Hemos usado tailwind para modificar nuestro layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        CONTACT PAGE
+    </x-slot:heading>
     <h1>CONTACT PAGE です.</h1>
 </x-layout>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        HOME PAGE
+    </x-slot:heading>
     <h1>HOME PAGE ですよう.</h1>
 </x-layout>
