71d2bfa101323e3e1a2003e2c09526bdf2c8b204
Jorda Coca <jorda.coca@estudiants.urv.cat>
2025-01-23 13:45:13 +0100

    Dia 9 completado Basicamente hemos estado usando Eloquent para hacer querys en la base de datos de forma sencilla, Además de eso hemos resulto problemas de nombres de las tablas devido a colisiones y hemos aprendido el como distinguir entre elementos no sensibles a ataques de moficiación a los que si

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,14 @@
 
 namespace App\Models;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
 // El namespace nos sirve para agrupar las clases para evitar las colisiones, es decir podemos tener 2 clases llamadas Jobs pero si estan en distintos namespaces NO sucede
 // Error
-class Job{
-    // Si ponemos ":" despues del metodo, es como especificamos el tipo de lo que devolvemos
-    public static function all():array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$100.000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Pro Boxer',
-                'salary' => '$60.000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Programmer',
-                'salary' => '$80.000'
-            ]
-            ];
-    }
+class Job extends Model{
+    protected $table = 'job_listings';
 
-    public static function find(int $id):array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);    // Lo hemos pasado aquí porque tiene más sentido operar sobre Job si estamos en la propia clase
-        // Controlamos el caso en que usuario no encuentre el trabajo
-        if(!$job){
-            abort(404);     // Mostramos el mensaje de que no lo hemos encontrado
-        }
-        return $job;
-    }
+    // Estos serán los 2 atributos pueden ser asignados de forma masiva, sirve para decir cuales pueden ser modificados por usuarios de forma multiple
+    // La gracia sería que las cosas que sean de seguridad que no sean fillable
+    protected $fillable = ['title', 'salary'];
 }
