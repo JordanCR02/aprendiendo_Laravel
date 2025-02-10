ad28ffed1a465fcfb100c201fe14809c1ddb88bc
Jorda Coca <jorda.coca@estudiants.urv.cat>
2025-02-10 19:03:03 +0100

    Dia 10 completado Hemos introducido el uso de las llaves foraneas, usamos para referenciar esos elementos los factories de dicha clase

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 // El namespace nos sirve para agrupar las clases para evitar las colisiones, es decir podemos tener 2 clases llamadas Jobs pero si estan en distintos namespaces NO sucede
 // Error
 class Job extends Model{
+    use HasFactory;
     protected $table = 'job_listings';
 
     // Estos serán los 2 atributos pueden ser asignados de forma masiva, sirve para decir cuales pueden ser modificados por usuarios de forma multiple
